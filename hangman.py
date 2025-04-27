8e33cd238dd460a274568c207057c7e8ea48d03e
byteauthority <154011817+byteauthority@users.noreply.github.com>
2025-04-27 07:18:18 +0200

    logic fix

--- hangman.py
+++ hangman.py
@@ -1,4 +1,4 @@
-g = 0 # guesses
+
 
 def def_word():
     the_word = (input("Enter the word/phrase to guess: ")).lower()
@@ -14,24 +14,24 @@ def def_word():
 
 def draw_hangman():
     print("""
-          ****|--------|********
-          ****|********|********
-          ****|********|********
-          ****|*******000*******
-          ****|******00000******
-          ****|*******000*******
-          ****|********|********
-          ****|***-----|------**
-          ****|********|********
-          ****|********|********
-          ****|********|********
-          ****|*******|*|*******
-          ****|******|***|******
-          ****|*****|*****|*****
-          ****|*****************
-          ****|*****************
-          ****|*****************
-          ****|*****************
+              |--------|        
+              |        |        
+              |        |        
+              |       000  
+              |      00000  
+              |       000  
+              |        |
+              |   -----|------  
+              |        |  
+              |        | 
+              |        |  
+              |       | |  
+              |      |   |  
+              |      |     |  
+              |  
+              |  
+              |  
+              |  
           
           \n\n""")
 
@@ -45,10 +45,11 @@ def game(word):
             empty.append('-')
     # new = ''.join(empty) # list to string
     # print(empty)
-    print(' '.join(empty))
+    print(f"{' '.join(empty)}\n\n")
     return empty #, new
     
 while True:
+    g = 0 # guesses
     usr = (def_word()).lower()
     
     if usr != 'bad':
@@ -73,19 +74,23 @@ while True:
                             blank[let_index] = letter
                             usr_l[let_index] = '0'
 
-                    print(' '.join(blank))
+                    print(f"{' '.join(blank)}\n\n")
                     if '_'not in blank:
-                        print(f"\n\n\nYOU WON!\n\nThe word is - {''.join(blank)}\n\n")
+                        print(f"\n\n\nYOU WON!\n\nThe word\phrase is: {usr}\n\n")
                     # print(usr_l)
+                elif letter in usr and letter in blank:
+                    print("\nThis letter is already guessed!\n")
                 else:
-                    print("Wrong letter\n\n")
-                    hang = 'HANGMAN\n'
+                    print("\nWrong letter\n\n")
+                    hang = 'HANGMAN'
                     g +=1
-                    print(hang[:g])
+                    print(f"{hang[:g]}\n\n")
                     
     
                     if g == 7:
                         draw_hangman()
+                        break
+                    
 
     else:
         print("Enter a real word please\n")
